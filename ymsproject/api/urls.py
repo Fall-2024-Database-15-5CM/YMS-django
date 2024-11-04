c4b412db64f5d5762d49332b687116aa6e556986
kim_mon_ey <113882868+kimmoney@users.noreply.github.com>
2024-11-04 15:54:38 +0900

    Update urls.py

--- ymsproject/api/urls.py
+++ ymsproject/api/urls.py
@@ -2,14 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('slot/yard-slot-info',views.get_yard_slot_info, name ='get_yard_slot_info'),
-    path('slot/updated-slot',views.get_updated_equipments,name='get_updated_equipments'),
+    path('livemap/yard-info',views.get_yard_slot_info, name ='get_yard_slot_info'),
+    path('livemap/updated',views.get_updated_equipments,name='get_updated_equipments'),
    
     
     
-    path('driver/sorted-drivers', views.get_sorted_drivers, name='get_sorted_drivers'),
+    path('driver/sorted', views.get_sorted_drivers, name='get_sorted_drivers'),
     path('driver/create', views.create_driver, name='create_driver'),
     path('driver/details-driver', views.get_driver_details, name='driver_details'),
+    
     
     path('equipment/equipment-details',views.get_equipment_details,name='get_equipment_details'),
     ## truck
